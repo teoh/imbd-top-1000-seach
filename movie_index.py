94851a9d21bafc853cad3bf5bf42403836a99d08
teoh <mteohh@gmail.com>
2018-09-10 10:24:23 -0700

    added documentation for movie_index

--- movie_index.py
+++ movie_index.py
@@ -17,6 +17,16 @@ class MovieIndex():
         self.keyword_to_indices = None
 
     def build(self):
+        '''This builds the search index of the movies.
+
+        Input: no input
+
+        Returns: nothing, but the calling object will
+            now have a list of movie titles, and dict
+            mapping possible keywords to indices (in the
+            movie title list) that are related to the
+            keyword
+        '''
         movie_info_list = []
         print('Getting info from IMDB pages and building index...')
         for i in tqdm(range(1, self.num_pages + 1)):
@@ -27,6 +37,18 @@ class MovieIndex():
         self.keyword_to_indices = self.make_keyword_map(movie_info_list)
 
     def search(self, search_string):
+        '''
+        Finds the relevant movies given a search
+        string query.
+
+        Input:
+            search_string: a space-separated string
+                of keywords
+
+        Returns:
+            list of titles related to the search string
+
+        '''
         list_of_terms = search_string.lower().split(' ')
         index_sets = list(filter(lambda idx_set: idx_set is not None,
             map(lambda term: self.keyword_to_indices.get(term,None),
@@ -40,6 +62,20 @@ class MovieIndex():
         return [self.titles[i] for i in relevant_indices]
 
     def make_keyword_map(self,movie_info_list):
+        '''
+        Creates a dictionary that maps keywords to
+        relevant movies.
+
+        Input:
+            movie_info_list: a list of dicts with fields
+                for genre, people, and title
+
+        Output:
+            keyword_map: {keyword -> {set of relevant movie
+                                        indices} }
+
+        '''
+
         keyword_map = {}
         for i, movie_info in enumerate(movie_info_list):
             for g in movie_info['genres']:
@@ -56,12 +92,30 @@ class MovieIndex():
         return keyword_map
 
     def add_to_dict(self,k_map,itm,i):
+        '''
+        Simple function to check for key
+        membership in a dict and add the value
+        accordingly
+
+        '''
         if itm not in k_map.keys():
             k_map[itm] = {i}
         else:
             k_map[itm].add(i)
 
     def get_movie_info_from_page(self, page_url):
+        '''Gets info on movies from page_url
+
+        Input:
+            page_url: url of page that we'll get
+                movies from
+
+        Output:
+            movies_info_from_page: list of dicts
+                with fields for genre, movie, and
+                title
+
+        '''
         response = get(page_url)
 
         html_soup = BeautifulSoup(response.text,'html.parser')
@@ -70,11 +124,25 @@ class MovieIndex():
         movies_info_from_page = []
 
         for movie in movie_containers:
+            print(type(movie))
             movies_info_from_page.append(self.extract_single_movie(movie))
 
         return movies_info_from_page
 
     def extract_single_movie(self, movie):
+        '''Extracts info for one movie
+
+        Input:
+            movie: a bs4.element.Tag object
+                describing information for one
+                movie
+
+        Output:
+            movie_res: dictionary with fields
+                describing genre, title, and
+                people for the movie
+
+        '''
         movie_res = {}
 
         # title
